b9d673ce25b7588eccc9b3571e765c9d72254ebe
Adrian John <adrianjohn6543@gmail.com>
2023-11-10 20:15:45 +0100

    add more log options

--- src/macros.rs
+++ src/macros.rs
@@ -1,12 +1,49 @@
+use crate::level::Level;
 #[macro_export]
 macro_rules! log {
     ($lvl:expr, $($arg:tt)+) => {
         $crate::log(format_args!($($arg)+),$lvl)
     };
 }
+
 #[macro_export]
 macro_rules! log_info {
     ($($arg:tt)+) => {
         log!(Level::INFO,$($arg)+)
+    };
+}
+
+#[macro_export]
+macro_rules! log_error {
+    ($($arg:tt)+) => {
+        log!(Level::ERROR,$($arg)+)
+    };
+}
+
+#[macro_export]
+macro_rules! log_trace {
+    ($($arg:tt)+) => {
+        log!(Level::TRACE,$($arg)+)
+    };
+}
+
+#[macro_export]
+macro_rules! log_debug {
+    ($($arg:tt)+) => {
+        log!(Level::DEBUG,$($arg)+)
+    };
+}
+
+#[macro_export]
+macro_rules! log_warn {
+    ($($arg:tt)+) => {
+        log!(Level::WARN,$($arg)+)
+    };
+}
+
+#[macro_export]
+macro_rules! log_error {
+    ($($arg:tt)+) => {
+        log!(Level::ERROR,$($arg)+)
     };
 }
