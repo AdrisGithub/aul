7c508a1dbe39ed9ac98b25ef4b38f270f4a02b9c
Adrian John <adrianjohn6543@gmail.com>
2023-11-10 20:01:42 +0100

    change it so the env variable gets checked every print statement

--- src/sensitive.rs
+++ src/sensitive.rs
@@ -1,15 +1,25 @@
 use std::fmt::{Display, Formatter};
 
-static SAFE_MODE: bool = std::env::var("SAFE_LOGGING").is_ok_and(|t| { t.eq("true") });
+const ENV_KEY: &'static str = "SAFE_LOGGING";
+const ENV_VALUE: &'static str = "true";
+const SAFE_PRINT: &'static str = "[REDACTED]";
 
 pub struct Sens<T>(T);
 
 impl<T> Display for Sens<T> where T: Display {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        if SAFE_MODE {
-            "[REDACTED]".fmt(f)
+        if self.is_safe() {
+            SAFE_PRINT.fmt(f)
         } else {
             self.0.fmt(f)
         }
     }
 }
+
+
+impl<T> Sens<T> {
+    fn is_safe(&self) -> bool {
+        std::env::var(ENV_KEY).is_ok_and(|t| { t.eq(ENV_VALUE) })
+    }
+}
+
