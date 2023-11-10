a8af8e1cab99d0d00cc38099798748df99e06b68
Adrian John <adrianjohn6543@gmail.com>
2023-11-10 21:53:46 +0100

    add errors.rs

---
+++ src/errors.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+
+static ERROR_MESSAGE: &str = "failed to convert a string that doesn't match a log level";
+
+#[derive(Debug)]
+pub struct ParseLogLevelError(());
+
+impl ParseLogLevelError {
+    pub const fn new() -> Self {
+        ParseLogLevelError(())
+    }
+}
+
+impl Error for ParseLogLevelError {}
+
+impl Display for ParseLogLevelError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(ERROR_MESSAGE)
+    }
+}
