5dd78bfb7f17fdfda99f8fca9cd8bafd8bd3488a
Adrian John <adrianjohn6543@gmail.com>
2023-11-10 19:29:46 +0100

    change make Sens struct public

--- src/sensitive.rs
+++ src/sensitive.rs
@@ -2,7 +2,7 @@ use std::fmt::{Display, Formatter};
 
 static SAFE_MODE: bool = std::env::var("SAFE_LOGGING").is_ok_and(|t| { t.eq("true") });
 
-struct Sens<T>(T);
+pub struct Sens<T>(T);
 
 impl<T> Display for Sens<T> where T: Display {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
