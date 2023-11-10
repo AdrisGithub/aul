d5b17e9bba724d4a6c17e13dca6462d7563c9e87
Adrian John <adrianjohn6543@gmail.com>
2023-11-10 22:09:23 +0100

    add documentation for ParseLogLevelError

--- src/errors.rs
+++ src/errors.rs
@@ -3,10 +3,17 @@ use std::fmt::{Display, Formatter};
 
 static ERROR_MESSAGE: &str = "failed to convert a string that doesn't match a log level";
 
+/// Error struct used and thrown when calling [`from_str`] or [`try_from`] on a String that doesn't contain a valid [`Level`]
+///
+/// [`Level`]: crate::Level
+/// [`from_str`]: std::str::FromStr::from_str
+/// [`try_from`]: TryFrom::try_from
 #[derive(Debug)]
 pub struct ParseLogLevelError(());
 
 impl ParseLogLevelError {
+    /// Constructs a new Instance of ParseLogLevelError
+    ///
     pub const fn new() -> Self {
         ParseLogLevelError(())
     }
