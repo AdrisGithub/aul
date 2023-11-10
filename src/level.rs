20ecab45a780dab0ac3a30f0b26f67b1f81d0e69
Adrian John <adrianjohn6543@gmail.com>
2023-11-10 20:12:44 +0100

    add unit tests for level.rs

--- src/level.rs
+++ src/level.rs
@@ -21,3 +21,26 @@ impl Level {
         NAMES[*self as usize]
     }
 }
+
+#[cfg(test)]
+mod tests{
+    use crate::level::Level;
+
+    #[test]
+    fn test_get_name(){
+        assert_eq!(Level::INFO.get_name(),"INFO")
+    }
+    #[test]
+    fn test_to_string(){
+        assert_eq!(Level::DEBUG.to_string(),"DEBUG")
+    }
+    #[test]
+    fn test_to_string_doesnt_equal_smth_else(){
+        assert_ne!(Level::WARN.to_string(),"DEBUG")
+    }
+    #[test]
+    fn test_get_name_doesnt_equal_smth_else(){
+        assert_ne!(Level::DEBUG.to_string(),"INFO")
+    }
+
+}
