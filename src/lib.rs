ccf4068a25a7dbf08f38856d5da4845152940392
Adrian John <adrianjohn6543@gmail.com>
2023-11-10 20:19:04 +0100

    add lib.rs to combine the logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,28 @@
+extern crate core;
+
+use std::fmt::Arguments;
+
+use crate::level::Level;
+
+pub mod level;
+pub mod sensitive;
+pub mod macros;
+
+
+fn log(args: Arguments, level: Level) {
+    println!("[{}]: {}", level, args)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::level::Level;
+    use crate::log;
+
+    #[test]
+    fn test_log_macro() {
+        log!(Level::INFO,"{}","Hello")
+    }
+}
+
+
 
